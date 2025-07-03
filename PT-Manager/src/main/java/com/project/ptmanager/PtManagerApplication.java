d63c6abc9cbf48f07d79d06b2e07613e45da06d1
escha-k <bbs7380@gmail.com>
2025-07-03 19:49:25 +0900

    Project init

---
+++ PT-Manager/src/main/java/com/project/ptmanager/PtManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.ptmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtManagerApplication.class, args);
+	}
+
+}
---
+++ PT-Manager/src/test/java/com/project/ptmanager/PtManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ptmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
