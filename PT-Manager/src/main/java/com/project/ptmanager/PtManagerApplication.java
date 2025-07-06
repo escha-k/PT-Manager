4c0081a09990bd73cecd24bd9770aca5d727901f
escha-k <bbs7380@gmail.com>
2025-07-06 22:17:55 +0900

    README 작성

--- PT-Manager/src/main/java/com/project/ptmanager/PtManagerApplication.java
+++ PT-Manager/src/main/java/com/project/ptmanager/PtManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PtManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PtManagerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PtManagerApplication.class, args);
+  }
 
 }
