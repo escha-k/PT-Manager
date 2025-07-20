635a3881777acd88b8f3f65910c0b016d2157041
escha-k <bbs7380@gmail.com>
2025-07-20 22:20:33 +0900

    fix: merge 실수로 삭제된 내용 복구

--- PT-Manager/src/main/java/com/project/ptmanager/repository/member/TrainerMemberMatchingRepository.java
+++ PT-Manager/src/main/java/com/project/ptmanager/repository/member/TrainerMemberMatchingRepository.java
@@ -1,11 +1,18 @@
 package com.project.ptmanager.repository.member;
 
 import com.project.ptmanager.domain.member.TrainerMemberMatching;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrainerMemberMatchingRepository extends JpaRepository<TrainerMemberMatching, Long> {
+public interface TrainerMemberMatchingRepository extends
+    JpaRepository<TrainerMemberMatching, Long> {
 
   Boolean existsByMemberIdAndTrainerId(Long memberId, Long trainerId);
+
+  Optional<TrainerMemberMatching> findByMemberId(Long memberId);
+
+  List<TrainerMemberMatching> findByTrainerId(Long trainerId);
 }
